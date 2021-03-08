74a6bc87b8cce6fe4201d9207cd92d6a7233ff8f
widiasih rambe <widiasihrambe@gmail.com>
2021-03-08 17:49:14 +0700

    coomit

---
+++ dasar_dartt/bin/dasar_dartt.dart
@@ -0,0 +1,5 @@
+import 'package:dasar_dartt/dasar_dartt.dart' as dasar_dartt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dasar_dartt.calculate()}!');
+}
---
+++ dasar_dartt/test/dasar_dartt_test.dart
@@ -0,0 +1,8 @@
+import 'package:dasar_dartt/dasar_dartt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
